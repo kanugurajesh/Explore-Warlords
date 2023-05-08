7440c5bd67b8ad95857320eef1146bcdb2e4565d
Rajesh <245122733172@mvsrec.edu.in>
2023-05-08 02:15:41 -0400

    adding the data

--- index.js
+++ index.js
@@ -4,4 +4,36 @@ var sound = new Howl({
     src: ['./songs/fight.mp3']
 });
 
-sound.play();
+// sound.play();
+
+// get all the images with class item-img
+
+const itemImg = document.querySelectorAll('.item-img');
+
+// get all the images with class item-img and add event listener
+
+itemImg.forEach(function (img) {
+    img.addEventListener('click', () => {
+        sound.stop();
+        sound = new Howl({
+            src: ['./songs/fight.mp3']
+        });
+        sound.play();
+    });
+
+    img.addEventListener('mouseover', () => {
+        sound.stop();
+        sound = new Howl({
+            src: ['./songs/fight.mp3']
+        });
+        sound.play();
+    });
+
+    img.addEventListener('mouseout', () => {
+        sound.stop();
+        sound = new Howl({
+            src: ['./songs/fight.mp3']
+        });
+        sound.stop();
+    });
+});
